53123f1bfa5afec8de063657d128bb5b02e9f4d4
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2021-09-28 20:52:06 +0300

    Descriptions added to about and services page

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,10 @@ import { Info, Layout, Leadership, SEOTags } from "../components";
 export default function AboutPage() {
   return (
     <Layout>
-      <SEOTags title="About" />
+      <SEOTags
+        title="About"
+        description="Quality pruning improves the health and appearance of trees, prolonging their useful life by removing undesirable branches, whether they are dead, weakened, diseased, or insect-infested."
+      />
       <Leadership />
       <Info />
     </Layout>
--- pages/services.js
+++ pages/services.js
@@ -3,7 +3,10 @@ import { Services, Layout, SEOTags } from "../components";
 export default function ServicesPage() {
   return (
     <Layout>
-      <SEOTags title="Services" />
+      <SEOTags
+        title="Services"
+        description="We take on jobs of all sizes from pruning, fertilizing, or removing one small tree to large-scale management or removal projects."
+      />
       <Services />
     </Layout>
   );
