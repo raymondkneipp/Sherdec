b26048434b76a1b290610087384fac3792787f48
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2021-09-29 13:39:11 +0300

    Moved viewport tag into _app.js since it will be used on all pages

--- components/SEOTags.jsx
+++ components/SEOTags.jsx
@@ -14,8 +14,6 @@ const SEOTags = ({ title, description }) => {
       <meta property="og:title" content={titleTemplate} />
       <meta name="twitter:title" content={titleTemplate} />
 
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-
       <meta
         name="description"
         content={description ? `${description}` : `${defaultDescription}`}
