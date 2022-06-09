a0aef5d59b9e49914e3bd97f3053f560467312d0
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-06-09 15:13:24 -0400

    Changed theme and removed 2 leaders

--- components/about/Leadership.jsx
+++ components/about/Leadership.jsx
@@ -2,51 +2,37 @@ import React from "react";
 import LeadershipItem from "./LeadershipItem";
 
 const Leadership = () => {
-  return (
-    <div className="container grid grid-flow-row xl:grid-flow-col gap-10 py-32">
-      <div className="flex flex-col justify-start items-start">
-        <h1 className="mb-5 text-4xl sm:text-5xl md:text-6xl font-bold">
-          Meet Our Leadership
-        </h1>
+	return (
+		<div className="container grid grid-flow-row xl:grid-flow-col gap-10 py-32">
+			<div className="flex flex-col justify-start items-start">
+				<h1 className="mb-5 text-4xl sm:text-5xl md:text-6xl font-bold">
+					Meet Our Leadership
+				</h1>
 
-        <p className="text-base-content opacity-70 xl:max-w-md mb-5 text-lg md:text-2xl">
-          Get to know the tree care experts.
-        </p>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 justify-items-start gap-10">
-        <LeadershipItem
-          name="Tod Miller"
-          title="Founder / CEO"
-          image="tod.jpg"
-        />
-        <LeadershipItem
-          name="Jerry Jones"
-          title="Marketing Manager"
-          image="placeholder.png"
-        />
-        <LeadershipItem
-          name="Mark Williams"
-          title="Finance Manager"
-          image="placeholder.png"
-        />
-        <LeadershipItem
-          name="John Doe"
-          title="Arborist"
-          image="placeholder.png"
-        />
-        <LeadershipItem
-          name="Cameron Barger"
-          title="Arborist"
-          image="placeholder.png"
-        />
-        <LeadershipItem
-          name="George Washington"
-          title="Arborist"
-          image="placeholder.png"
-        />
-      </div>
-    </div>
-  );
+				<p className="text-base-content opacity-70 xl:max-w-md mb-5 text-lg md:text-2xl">
+					Get to know the tree care experts.
+				</p>
+			</div>
+			<div className="grid grid-cols-1 md:grid-cols-2 justify-items-start gap-10">
+				<LeadershipItem name="Tod Miller" title="Owner" image="tod.jpg" />
+				<LeadershipItem
+					name="Jerry Jones"
+					title="Arborist"
+					image="placeholder.png"
+				/>
+				<LeadershipItem
+					name="Mark Williams"
+					title="Arborist"
+					image="placeholder.png"
+				/>
+				<LeadershipItem
+					name="Samual King"
+					title="Arborist"
+					image="placeholder.png"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Leadership;
--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,29 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
+	static async getInitialProps(ctx) {
+		const initialProps = await Document.getInitialProps(ctx);
+		return { ...initialProps };
+	}
 
-  render() {
-    return (
-      <Html data-theme="garden" lang="en">
-        <Head>
-          <meta charSet="utf-8" />
+	render() {
+		return (
+			<Html data-theme="emerald" lang="en">
+				<Head>
+					<meta charSet="utf-8" />
 
-          <meta name="theme-color" content="#E9E7E7" />
+					<meta name="theme-color" content="#E9E7E7" />
 
-          <link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
-          <link rel="icon" type="image/png" href="/images/favicon.png" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+					<link rel="icon" type="image/svg+xml" href="/images/favicon.svg" />
+					<link rel="icon" type="image/png" href="/images/favicon.png" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
